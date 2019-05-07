2bc5ce743fd83aaf7c1ee1beb3daf32b0487ac11
ningqian <ningqian2199@users.noreply.github.com>
2019-05-08 00:27:12 +0800

    最新提交

---
+++ springboot-test/src/main/java/com/mvn/springboot/SpringbootTestApplication.java
@@ -0,0 +1,21 @@
+package com.mvn.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTestApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String index(){
+        return "欢迎来到SpringBoot";
+    }
+
+}
